762f0865adc9d55eca6b5841f61dc5ec27515d35
anusca <anna.arsentieva@gmail.com>
2020-06-05 15:24:43 -0700

    add morgan package

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const routes = require("./routes.js");
 
 const app = express();
 app.set("view engine", "pug");
+
+app.use(morgan("dev"));
 app.use(routes);
 
 //TODO
